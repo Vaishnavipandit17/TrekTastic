a7b5a347f2c58fffd067f3f491eb9e7c16b7bf84
Vaishnavi Pandit <104513236+Vaishnavipandit17@users.noreply.github.com>
2023-06-06 17:25:23 +0530

    Initial commit

---
+++ src/routes/Home.js
@@ -0,0 +1,27 @@
+import Destination from "../components/Destination";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Trip from "../components/Trip";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero
+        cName="hero"
+        heroImg="https://wallpaperaccess.com/full/1318238.jpg"
+        title="Your Journey Your Story"
+        text="Choose Your Favorite Destination."
+        buttonText="Travel Plan"
+        url="/"
+        btnClass="show"
+      />
+      <Destination />
+      <Trip />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
